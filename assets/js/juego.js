6a82404f36549349b1b350c197a7a58d6f79d83a
Angelo <alvaradoleon59@gmail.com>
2025-03-24 16:28:25 -0700

    implement game reset functionality and update event listeners in Blackjack

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -12,10 +12,20 @@ const especiales = ['A', 'J', 'Q', 'K'];
 let puntosJugador = 0,
     puntosComputadora = 0;
 
-//Referencias del HTML
+//* Referencias del HTML
+// Botones
 const btnPedir = document.querySelector('#btnPedir');
+const btnDetener = document.querySelector('#btnDetener');
+const btnNuevoJuego = document.querySelector('#btnNuevo');
+
+// Puntos
 const puntosHTML = document.querySelectorAll('small');
 
+// Cartas
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector('#computadora-cartas');
+
+btnDetener.disabled = true;
 
 // Esta función crea una nueva baraja
 const crearDeck = () => {
@@ -39,6 +49,29 @@ const crearDeck = () => {
 
 crearDeck();
 
+const reiniciarJuego = () => {
+
+    btnNuevoJuego.disabled = true;
+
+    // Reiniciar variables
+    deck = [];
+    deck = crearDeck();
+    puntosJugador = 0;
+    puntosComputadora = 0;
+
+    // Actualizar interfaz
+    puntosHTML[0].innerText = '0';
+    puntosHTML[1].innerText = '0';
+
+    // Limpiar cartas
+    divCartasJugador.innerText = '';
+    divCartasComputadora.innerText = '';
+
+    // Restablecer estado de botones
+    btnPedir.disabled = false;
+    btnDetener.disabled = true;
+}
+
 // Esta función me permite tomar una carte
 
 const pedirCarta = () => {
@@ -52,7 +85,6 @@ const pedirCarta = () => {
 }
 
 // pedirCarta();
-
 const valorCarta = (carta) => {
     const valor = carta.substring(0, carta.length - 1);
 
@@ -63,13 +95,87 @@ const valorCarta = (carta) => {
     }
 }
 
+// Turno de la computadora
+const turnoComputadora = ( puntosMinimos ) => {
+
+    do {
+        const carta = pedirCarta();
+
+        puntosComputadora += valorCarta( carta );
+
+        // Seleccionar la imagen de la carta
+        puntosHTML[1].innerText = puntosComputadora;
+
+        // Crear imagen de la carta
+        const imgCarta = document.createElement('img');
+        imgCarta.src = `assets/cartas/${ carta }.png`;
+        imgCarta.classList.add('carta');
+        divCartasComputadora.append( imgCarta );
+
+        if ( puntosMinimos > 21 ) {
+            break;
+        }
+    } while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) );
+
+    setTimeout(() => {
+        if ( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana');
+        } else if ( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana');
+        }
+    }, 100);
+}
+
 // Eventos
 btnPedir.addEventListener('click', () => {
+
+    btnDetener.disabled = false;
+    btnNuevoJuego.disabled = false;
 
     const carta = pedirCarta();
 
     puntosJugador += valorCarta( carta );
 
+    // Seleccionar la imagen de la carta
     puntosHTML[0].innerText = puntosJugador;
 
+    // Crear imagen de la carta
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${ carta }.png`;
+    imgCarta.classList.add('carta');
+    divCartasJugador.append( imgCarta );
+
+    if (puntosJugador > 21) {
+        console.warn('Lo siento mucho, perdiste');
+        btnPedir.disabled = true;
+        btnDetener.disabled = true;
+        turnoComputadora( puntosJugador );
+    } else if (puntosJugador === 21) {
+        console.warn('21, genial!');
+        btnPedir.disabled = true;
+        turnoComputadora(puntosJugador);
+    }
 });
+
+btnDetener.addEventListener('click', () => {
+
+    // Desactivar botones
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+
+    // Iniciar el turno de la computadora
+    turnoComputadora(puntosJugador);
+
+});
+
+btnNuevoJuego.addEventListener('click', () => {
+    console.clear()
+    reiniciarJuego();
+
+});
+
+
